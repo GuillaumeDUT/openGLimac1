096eb8d8ba13f74c90afe2020bed08f89ac36ed5
Guillaume DUT <GuillaumeDUT@github.com>
2018-03-01 14:41:47 +0100

    TP2 exo2

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#define SEGMENTSCERCLES 64
 
 /* Dimensions de la fenêtre */
 static unsigned int WINDOW_WIDTH = 800;
@@ -15,110 +17,6 @@ static const unsigned int BIT_PER_PIXEL = 32;
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
-typedef struct Point {
-    float x, y;
-    unsigned char r, g, b;
-    struct Point* next;
-} Point, *PointList;
-
-Point* allocPoint(float x, float y, unsigned char r, unsigned char g, unsigned char b) {
-    /*
-    On alloue un espace mémoire suffisant pour pouvoir stocker un point
-    Attention : la fonction malloc() renvoie un void* qu'il faut impérativement caster en Point*.
-    */
-    Point* point = (Point*) malloc(sizeof(Point));
-    if (!point) {
-        return NULL;
-    }
-    point->x = x;
-    point->y = y;
-    point->r = r;
-    point->g = g;
-    point->b = b;
-    point->next = NULL;
-    return point;
-}
-
-void addPointToList(Point* point, PointList* list) {
-    assert(point);
-    assert(list);
-    if (*list == NULL) {
-        /* Si le pointeur sur la liste est vide, on le modifie pour le faire pointer sur le point donné en paramètre */
-        *list = point;
-    } else {
-        /* Sinon, on réapplique la même fonction mais sur le point suivant */
-        addPointToList(point, &(*list)->next);
-    }
-}
-
-void drawPoints(PointList list) {
-    while(list) {
-        glColor3ub(list->r, list->g, list->b);
-        glVertex2f(list->x, list->y);
-        list = list->next;
-    }
-}
-
-void deletePoints(PointList* list) {
-    assert(list);
-    while (*list) {
-        Point* next = (*list)->next;
-        free(*list);
-        *list = next;
-    }
-}
-
-typedef struct Primitive{
-    GLenum primitiveType;
-    PointList points;
-    struct Primitive* next;
-} Primitive, *PrimitiveList;
-
-Primitive* allocPrimitive(GLenum primitiveType) {
-    /*
-    On alloue un espace mémoire suffisant pour pouvoir stocker une primitive
-    Attention : la fonction malloc() renvoie un void* qu'il faut impérativement caster en Primitive*.
-    */
-    Primitive* primitive = (Primitive*) malloc(sizeof(Primitive));
-    if (!primitive) {
-        return NULL;
-    }
-    primitive->primitiveType = primitiveType;
-    primitive->points = NULL;
-    primitive->next = NULL;
-    return primitive;
-}
-
-void addPrimitive(Primitive* primitive, PrimitiveList* list) {
-    assert(primitive);
-    assert(list);
-    if (*list == NULL) {
-        // Si le pointeur sur la liste est vide, on le modifie pour le faire pointer sur la primitive donnée en paramètre
-        *list = primitive;
-    } else {
-        // Sinon, on réapplique la même fonction mais sur le point suivant
-        addPrimitive(primitive, &(*list)->next);
-    }
-}
-
-void drawPrimitives(PrimitiveList list) {
-    while(list) {
-        glBegin(list->primitiveType);
-        drawPoints(list->points);
-        glEnd();
-        list = list->next;
-    }
-}
-
-void deletePrimitive(PrimitiveList* list) {
-    assert(list);
-    while(*list) {
-        Primitive* next = (*list)->next;
-        deletePoints(&(*list)->points);
-        free(*list);
-        *list = next;
-    }
-}
 
 void resizeViewport() {
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -128,32 +26,39 @@ void resizeViewport() {
     SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
 }
 
+void drawSquare(int full){
+  glColor3ub(255,255,255);
+  GLenum type = full ? GL_QUADS : GL_LINE_LOOP;
+  glBegin(type);
+    glVertex2f(0.5,0.5);
+    glVertex2f(0.5,-0.5);
+    glVertex2f(-0.5,-0.5);
+    glVertex2f(-0.5,0.5);
+  glEnd();
+}
 
-static const unsigned char COLORS[] = {
-    255, 255, 255,
-    0, 0, 0,
-    255, 0, 0,
-    0, 255, 0,
-    0, 0, 255,
-    255, 255, 0,
-    0, 255, 255,
-    255, 0, 255
-};
+void drawLandMark(){
+  glColor3ub(255,0,0);
+  glBegin(GL_LINES);
+    glVertex2f(-1,0);
+    glVertex2f(1,0);
+  glEnd();
 
-static const unsigned int NB_COLORS = sizeof(COLORS) / (3 * sizeof(unsigned char));
+  glColor3ub(0,255,0);
+  glBegin(GL_LINES);
+    glVertex2f(0,1);
+    glVertex2f(0,-1);
+  glEnd();
+}
 
-void drawColorPalette() {
-    int i;
-    float dx = 2.f / NB_COLORS; // dx = "delta x"
-    glBegin(GL_QUADS);
-    for(i = 0; i < NB_COLORS; ++i) {
-        glColor3ubv(COLORS + i * 3);
-        glVertex2f(-1 + i * dx, -1);
-        glVertex2f(-1 + (i + 1) * dx, -1);
-        glVertex2f(-1 + (i + 1) * dx, 1);
-        glVertex2f(-1 + i  * dx, 1);
+void drawCircle(int full){
+  glColor3ub(0,0,0);
+  GLenum type = full ? GL_POLYGON : GL_LINE_LOOP;
+  glBegin(type);
+    for(int i=0;i<SEGMENTSCERCLES;i++){
+      glVertex2f(cos(i*(2*M_PI/SEGMENTSCERCLES))/2,sin(i*(2*M_PI/SEGMENTSCERCLES))/2);
     }
-    glEnd();
+  glEnd();
 }
 
 int main(int argc, char** argv) {
@@ -169,22 +74,13 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
-    SDL_WM_SetCaption("Paint IMAC", NULL);
+    SDL_WM_SetCaption("J'VEUX DU L J'VEUX DU V j'VEUX DU G", NULL);
 
     glClearColor(0.1, 0.1, 0.1, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* On créé une première primitive par défaut */
-    PrimitiveList primitives = NULL;
-    Primitive* prim = allocPrimitive(GL_POINTS);
-    PrimitiveList lastPrim = prim;
-    addPrimitive(prim, &primitives);
-
-    int loop = 1;
-    int mode = 0; // le mode d'affichage. 0 = le dessin, 1 = la palette
-    unsigned int currentColor = 0; // l'index de la couleur courante dans le tableau COLORS
-    GLenum currentPrimitiveType = GL_POINTS;
-
+    int loop=1;
     /* Boucle d'affichage */
     while(loop) {
 
@@ -195,13 +91,9 @@ int main(int argc, char** argv) {
 
         glClear(GL_COLOR_BUFFER_BIT); // Toujours commencer par clear le buffer
 
-        if (mode == 0) {
-            drawPrimitives(primitives); // On dessine la liste de primitives
-        }
-        else if (mode == 1) {
-            drawColorPalette();
-        }
-
+        drawSquare(0);
+        drawLandMark();
+        drawCircle(0);
         /* Boucle traitant les evenements */
         SDL_Event e;
         while(SDL_PollEvent(&e)) {
@@ -211,90 +103,12 @@ int main(int argc, char** argv) {
                 loop = 0;
                 break;
             }
+              switch (e.type) {
 
-            /* Quelques exemples de traitement d'evenements : */
-            switch(e.type) {
 
-                /* Clic souris */
-                case SDL_MOUSEBUTTONUP:
-                    if (mode == 1) {
-                        /* On retrouve automatiquement l'index de la couleur cliquée en connaissant la taille de notre tableau */
-                        currentColor = e.button.x * NB_COLORS / WINDOW_WIDTH;
-                    }
-                    else if (mode == 0) {
-                        /* Transformation des coordonnées du clic souris en coordonnées OpenGL */
-                        float x = -1 + 2. * e.button.x / WINDOW_WIDTH;
-                        float y = -(-1 + 2. * e.button.y / WINDOW_HEIGHT);
-                        /* On retrouve la couleur courante grace à son index */
-                        unsigned int r = COLORS[currentColor * 3];
-                        unsigned int g = COLORS[currentColor * 3 + 1];
-                        unsigned int b = COLORS[currentColor * 3 + 2];
-                        /* On ajoute un nouveau point à la liste de la primitive courante */
-                        addPointToList(allocPoint(x, y, r, g, b), &lastPrim->points);
-                    }
-                    break;
-
-                /* Touche clavier */
-                case SDL_KEYDOWN:
-
-                    printf("touche pressée (code = %d)\n", e.key.keysym.sym);
-
-                    if (e.key.keysym.sym == SDLK_SPACE) {
-                        mode = 1;
-                    }
-
-                    int newPrimitivePressed = 0;
-                    GLenum newPrimitiveType;
-
-                    switch(e.key.keysym.sym) {
-
-                        case SDLK_q:
-                            loop = 0;
-
-                        case SDLK_SPACE:
-                            mode = 1;
-                            break;
-
-                        case SDLK_p:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_POINTS;
-                            break;
-
-                        case SDLK_l:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_LINES;
-                            break;
-
-                        case SDLK_t:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_TRIANGLES;
-                            break;
-
-                        case SDLK_c:
-                            /* Touche pour effacer le dessin */
-                            deletePrimitive(&primitives); // on supprime les primitives actuelles
-                            addPrimitive(allocPrimitive(currentPrimitiveType), &primitives); // on réinitialise à la primitive courante
-                            break;
-
-                        default:
-                            break;
-                    }
-
-                    if (newPrimitivePressed && currentPrimitiveType != newPrimitiveType) {
-                        prim = allocPrimitive(newPrimitiveType);
-                        lastPrim = prim;
-                        addPrimitive(prim, &primitives);
-                        currentPrimitiveType = newPrimitiveType;
-                    }
+                case SDL_MOUSEBUTTONDOWN:
 
                     break;
-
-                case SDL_KEYUP:
-                    if (e.key.keysym.sym == SDLK_SPACE) {
-                        mode = 0;
-                    }
-                    break;
-
                 case SDL_VIDEORESIZE:
                     WINDOW_WIDTH = e.resize.w;
                     WINDOW_HEIGHT = e.resize.h;
@@ -303,6 +117,7 @@ int main(int argc, char** argv) {
                 default:
                     break;
             }
+
         }
 
         /* Echange du front et du back buffer : mise à jour de la fenêtre */
@@ -316,8 +131,6 @@ int main(int argc, char** argv) {
             SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
         }
     }
-
-    deletePrimitive(&primitives);
 
     /* Liberation des ressources associées à la SDL */
     SDL_Quit();
